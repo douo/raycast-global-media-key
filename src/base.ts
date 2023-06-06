a5dc223e02fc28a43f2cc4cb1b0a5e7aec8f18a9
tiou <dourokinga@gmail.com>
2023-06-06 17:23:11 +0800

    fix: blocking forever when triggered by shortcut

--- src/base.ts
+++ src/base.ts
@@ -3,28 +3,30 @@ import {
   environment,
   showHUD,
 } from "@raycast/api";
-import { spawnSync } from "child_process";
+import { spawnSync, execSync } from "child_process";
 import fs from "fs";
 
 
 export default async function send(type: string) {
   console.log(`send:${type}`)
-  await closeMainWindow({ clearRootSearch: true });
+  // https://raycastcommunity.slack.com/archives/C02HEMAF2SJ/p1686042687713179
+  closeMainWindow({ clearRootSearch: true });
   const binary = `${environment.assetsPath}/media-key`;
+  console.log(binary)
   try {
     await fs.promises.access(binary, fs.constants.X_OK);
   } catch {
     await fs.promises.chmod(binary, 0o775);
   }
-  console.log(binary)
-  const { status, output, stdout, stderr, error } = spawnSync(binary, [type], {
-    detached: true,
-    stdio: 'ignore',
-  });
   showHUD(`send Key: ${type}`)
-  console.log(status)
-  console.log(output)
-  console.log(error)
-  console.log(stdout?.toString())
-  console.log(stderr?.toString())
+  _exec(binary, type)
+
+}
+
+function _exec(binary, type){
+  // const { status, output, stdout, stderr, error } = spawnSync(binary, [type], {
+  //   detached: true,
+  //   stdio: 'ignore',
+  // });
+  execSync(`${binary} ${type}`)
 }
